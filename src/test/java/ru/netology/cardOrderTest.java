da31588a79c9e45becde1ec37bbf26fe006246e9
Alexander Khyrbu <alexander.khyrbu@list.ru>
2024-05-13 23:22:42 +0300

    add logic

--- src/test/java/ru/netology/cardOrderTest.java
+++ src/test/java/ru/netology/cardOrderTest.java
@@ -1,4 +1,50 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class cardOrderTest {
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999");
+        WebElement from = driver.findElement(By.cssSelector("[class]from"));
+        from.findElement(By.cssSelector("[type=button]")).click();
+        from.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys();
+        String text = from.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("", text.trim());
+    }
+
 }
